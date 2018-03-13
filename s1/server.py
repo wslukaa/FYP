beec5b930d0dd310ee1feab71b3620d52aa2338c
ChiuSing Tsang <cstsangac@connect.ust.hk>
2018-03-13 16:59:20 +0800

    Add files via upload

--- s1/server.py
+++ s1/server.py
@@ -1,35 +1,50 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
 import json
- 
+import re 
+
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
-tags = []
+
+data = {}
 with open('raw_plus_preprocessed_multiple_3') as f:
 	for line in f:
 		tag = json.loads(line)
-		tags.append(tag)
+		ht_name = tag['ht_name']
+		ts_start = tag['ts_start']
+		tag_name = ht_name + '-' + ts_start
+		tag_name = re.sub('#','_',tag_name)
+		data[tag_name] = tag
+		
+#<a href=""></a>
+def get_list_of_samples():
+	l = {}
+	for k,v in data.items():
+		tag_name = v['ht_name'] + '-' + v['ts_start']
+		l[k] = {}
+		l[k]['tag_name'] = tag_name
+		l[k]['isTrend'] = v['isTrend']
+	return l
 
-def getExchangeRates():
-    rates = [1.1,2.1,3.1,4.1]
-    return rates
- 
-@app.route("/")
-def index():
-	ht_name = tags[0]['ht_name']
-	ts_start = tags[0]['ts_start']
+@app.route('/tag/<tag_name>')
+def render_tag(tag_name):
+	list_of_samples = get_list_of_samples()
+
+	tag = data[tag_name]
+	ht_name = tag['ht_name']
+	ts_start = tag['ts_start']
 	tag_name = ht_name + '-' + ts_start
-	isTrend = tags[0]['isTrend']
+	isTrend = tag['isTrend']
 	df_raw = [['timestamp', 'value']]
 	df_nor = [['timestamp', 'value']]
 	df_em = [['timestamp', 'value']]
 	df_smoothed = [['timestamp', 'value']]
 
-	ts_full = tags[0]['ts_full']
-	_df_raw = tags[0]['df_raw']
-	_df_nor = tags[0]['df_nor']
-	_df_em = tags[0]['df_em']
-	_df_smoothed = tags[0]['df_smoothed']
+	ts_full = tag['ts_full']
+	_df_raw = tag['df_raw']
+	_df_nor = tag['df_nor']
+	_df_em = tag['df_em']
+	_df_smoothed = tag['df_smoothed']
 
 	for i in range(0,len(ts_full)):
 		df_raw.append([ts_full[i],_df_raw[i]])
@@ -37,7 +52,11 @@ def index():
 		df_em.append([ts_full[i],_df_em[i]])
 		df_smoothed.append([ts_full[i],_df_smoothed[i]])
 
-	return render_template('test.html',**locals())      
+	return render_template('test.html',**locals())
+
+@app.route("/")
+def index():
+	return render_tag('_crypto-20180206-145600')
  
 @app.route("/hello")
 def hello():
